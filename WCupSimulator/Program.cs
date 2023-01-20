cda974547e67a722e11a9d1f22dcac3aaf11a850
EricFG <eric_fg@live.com.ar>
2023-01-20 13:35:13 -0300

    update api

--- WCupSimulator/Program.cs
+++ WCupSimulator/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using WCupSimulator;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<WCPContext>(options => 
+        options.UseSqlServer(builder.Configuration.GetConnectionString("SrvConn")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -10,24 +16,56 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/teams", async () =>
+app.MapGet("/teams/{id}", async (WCPContext context, Guid id) =>
 {
+    var team = await context.Teams.FindAsync(id);
+
+    return Results.Ok(team);
 
 });
 
-app.MapPost("/teams", async () =>
+app.MapGet("/teams", async (WCPContext context) =>
 {
+    var teams = await context.Teams.ToListAsync();
 
+    return Results.Ok(teams);
+    
 });
 
-app.MapPut("/teams", async () =>
+app.MapPost("/teams", async (WCPContext context, Team team) =>
 {
+    await context.Teams.AddAsync(team);
+    await context.SaveChangesAsync();
 
+    return Results.Ok(team);
+    
 });
 
-app.MapDelete("/teams", async () =>
+app.MapPut("/teams/{id}", async (WCPContext context, Team team) =>
 {
+    var dbTeam = await context.Teams.FindAsync(team.Id);
+    if (dbTeam == null)
+        return Results.NotFound();
 
+    dbTeam.Name = team.Name;
+    dbTeam.Img = team.Img;
+
+    context.Teams.Update(dbTeam);
+    await context.SaveChangesAsync();
+
+    return Results.Ok(dbTeam);
+});
+
+app.MapDelete("/teams/{id}", async (WCPContext context, Guid id) =>
+{
+    var dbTeam = await context.Teams.FindAsync(id);
+    if (dbTeam == null)
+        return Results.NotFound();
+
+    context.Teams.Remove(dbTeam);
+    await context.SaveChangesAsync();
+
+    return Results.NoContent();
 });
 
 app.Run();
---
+++ WCupSimulator/WCPContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace WCupSimulator
+{
+    public class WCPContext : DbContext
+    {
+        public WCPContext(DbContextOptions options) 
+            : base(options) 
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("SrvConn"));
+        }
+
+        public DbSet<Team> Teams { get; set; }
+    }
+}
