0f47b8f54216ea4f6c1f540496d13b5d6eccf99a
EricFG <eric_fg@live.com.ar>
2023-01-21 18:58:52 -0300

    update layout #1

--- WCupLayout/wcup-simulator-web/src/app/groups/groups.component.ts
+++ WCupLayout/wcup-simulator-web/src/app/groups/groups.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { moveItemInArray } from '@angular/cdk/drag-drop'
+import { Team } from '../models/Team';
+
 
 @Component({
   selector: 'app-groups',
   templateUrl: './groups.component.html',
   styleUrls: ['./groups.component.scss']
 })
-export class GroupsComponent {
+
+
+export class GroupsComponent implements OnInit {
+
+  constructor() { }
+    groups!: Team[][];
+
+    ngOnInit(): void {
+      this.initGroup()
+    }
+    
+    initGroup() {
+      this.groups = [];
+      for(var i=0; i < 8; i++) {
+        this.groups[i] = [
+          {name: '', img: ''},
+          {name: '', img: ''},
+          {name: '', img: ''},
+          {name: '', img: ''}
+        ]
+      }
+  
+    }
+
+    drop(event:any, index: number) {
+      moveItemInArray(this.groups[index], event.previousIndex, event.currentIndex)
+      
+    }
 
 }
